5295d1b7b6b3fd27fb289c6c3c10b24763a88857
Pawel Pieczul <pieczul@gmail.com>
2019-07-13 10:51:55 +0200

    Added component IDs to the image
    Signed-off-by: Pawel Pieczul <pieczul@gmail.com>

--- a3-board.py
+++ a3-board.py
@@ -25,11 +25,14 @@
 # Author: Pawel Pieczul
 #
 
-import sys, re, json, getopt, fnmatch, matplotlib
+import sys, re, json, getopt, fnmatch, matplotlib, numpy
 from json import JSONDecodeError
 from matplotlib import image
 from matplotlib import pyplot
 from matplotlib.patches import Rectangle
+from matplotlib.text import TextPath
+from matplotlib.patches import PathPatch
+from matplotlib.transforms import Affine2D, IdentityTransform
 
 global prog_name
 
@@ -40,6 +43,30 @@ class MotherBoard:
 			return
 		self.components = json["components"]
 		self.traces = json["traces"]
+
+def usage():
+	print()
+	print("USAGE:", prog_name, " -c<ids>|-t<ids> [options]")
+	print()
+	print("OPTIONS:")
+	print("  {:<33} {}".format("-c,--component <id1>[,<id2>,...]", "Display components (wildcards allowed for each ID)"))
+	print("  {:<33} {}".format("-g,--graphics", "Display board image with annotations"))
+	print("  {:<33} {}".format("-h,--help", "Display help"))
+	print("  {:<33} {}".format("-j,--json <json>", "Use a given JSON file with wire list"))
+	print("  {:<33} {}".format("-s,--sequential", "Display traces for each component separately"))
+	print("  {:<33} {}".format("-t,--trace <id1>[,<id2>,...]", "Display traces (wildcards allowed for each ID)"))
+	print()
+	print("EXAMPLES:")
+	print("  {} -c* -s".format(prog_name))
+	print("      Display all components and all traces one by one.")
+	print()
+	print("  {} -cC*".format(prog_name))
+	print("      Display all capacitors and a combined trace.")
+	print()
+	print("  {} -cU160,R5,X*".format(prog_name))
+	print("      Display chip U160, resistor R5 and all diodes and a combined trace.")
+	print()
+	sys.exit()
 
 def is_id_power(id):
 	return id in ("GND", "+12V", "-12V", "+5V", "-5V", "+12FV", "-12FV", "+5FV", "-5FV", "GNDF")
@@ -67,80 +94,64 @@ def format_pins(pins):
 		txt += "-" if trace == "" else trace
 	return " (" + txt + ")"
 
-def format_trace(id, trace, id_width):
+def format_component(id, c, id_width, part_width, single_mode, sequential_filter):
+	return "{}: {} {}{}".format( \
+		id.rjust(id_width),  \
+		(c["part"] + ("" if c["type"] == "" else "/" + c["type"])).ljust(part_width), \
+		c["location"].ljust(3), "" if single_mode or sequential_filter else format_pins(c["pins"]))
+
+def format_trace(id, trace, id_width, single_mode):
 	if is_id_power(id):
 		return "{}: ...".format(id.rjust(id_width))
 	else:
-		trace.sort(key = lambda x: x[0] + str(x[1]).zfill(2))
+		trace.sort(key = lambda x: x[0][0] + x[0][1:].zfill(3) + str(x[1]).zfill(2))
 		txt = ""
 		for t in trace:
 			pin = "{}-{}".format(t[0], t[1] + 1)
-			txt += "{:<8}".format(pin)
+			txt += ("{} " if single_mode else "{:<8}").format(pin)
 		return "{}: {}".format(id.rjust(id_width), txt)
 
-def usage():
-	print()
-	print("USAGE:", prog_name, "[options]")
-	print()
-	print("OPTIONS:")
-	print("  {:<33} {}".format("-c,--component <id1>[,<id2>,...]", "Display given component IDs (wildcards allowed for each ID)"))
-	print("  {:<33} {}".format("-g,--graphics", "Display board image with annotations"))
-	print("  {:<33} {}".format("-h,--help", "Display help"))
-	print("  {:<33} {}".format("-j,--json <json>", "Use a given JSON file with wire list"))
-	print("  {:<33} {}".format("-s,--sequential", "Display traces for each component separately"))
-	print()
-	print("EXAMPLES:")
-	print("  {} -c* -s".format(prog_name))
-	print("      Display all components and all traces one by one.")
-	print()
-	print("  {} -cC*".format(prog_name))
-	print("      Display all capacitors and a combined trace.")
-	print()
-	print("  {} -cU160,R5,X*".format(prog_name))
-	print("      Display chip U160, resistor R5 and all diodes and a combined trace.")
-	print()
-	sys.exit()
+def draw_text(txt, x, y, w, h, gca):
+	rotate = (w < h)
+	tp = TextPath((0, 0), txt, size = (w if rotate else h) * 0.7)
+	tbox = tp.get_extents()
+	if not rotate:
+		xtr = x + (w - tbox.width) / 2
+		ytr = y - h + (h - tbox.height) / 2
+		tp = tp.transformed(Affine2D().translate(xtr, ytr))
+	else:
+		xtr = x + tbox.height + (w - tbox.height) / 2
+		ytr = y - h + (h - tbox.width) / 2
+		tp = tp.transformed(Affine2D().rotate_deg(90) + Affine2D().translate(xtr, ytr))
+	txt = PathPatch(tp, linewidth=0.5, facecolor = "0", edgecolor = "0")
+	gca.add_patch(txt)
 
-def main(argv):
-	try:
-		opts, args = getopt.getopt(argv, "ghj:c:s",["graphics", "help","json=","component=","sequential"])
-	except getopt.GetoptError:
-		usage()
+def draw_component(id, c, gca):
+	if gca is not None and "box" in c:
+		colors = { "U" : "#00ff00", \
+				   "L" : "#ff0000", \
+				   "J" : "#ffff00", \
+				   "Q" : "#ff0000", \
+				   "P" : "#ff00ff"}
+		b = c["box"]; t = id[0]
+		x = b[0]; y = 2000 - b[1]; w = b[2]; h = b[3]
+		color = colors[t] if t in colors else "#000000"
+		rect = Rectangle((x, y), w, h, linewidth = 1.5, edgecolor = color, facecolor = color + "40")
+		gca.add_patch(rect)
+		draw_text(id, x, y, abs(w), abs(h), gca)
 
-	if len(opts) == 0:
-		usage()
-
-	json_file = "./original/a3-wire-list.json"
-	component_filter = "*"
-	sequential_filter = False
-	graphics = False
-
-	for opt, arg in opts:
-		if opt in ["-h", "--help"]:
-			usage()
-		elif opt in ["-c", "--component"]:
-			component_filter = arg.upper()
-		elif opt in ["-j", "--json"]:
-			json_file = arg
-		elif opt in ["-s", "--sequential"]:
-			sequential_filter = True
-		elif opt in ["-g", "--graphics"]:
-			graphics = True
-
-	board = load_json(json_file)
-	if board is None: 
-		return
-
+def print_components(board, component_filter, sequential_filter, gca):
 	print()
 	filters = re.split(",", component_filter)
 	components = [board.components[key] for key in board.components.keys() \
 					if any([fnmatch.fnmatch(key, filter) for filter in filters])]
+	if len(components) == 0:
+		return
+	components.sort(key = lambda x: x["id"][0] + x["id"][1:].zfill(4))
+
 	traces = {key : board.traces[key] for key in board.traces.keys() \
 				if any([any([fnmatch.fnmatch(v, filter) for v in [t[0] for t in board.traces[key]]]) \
 						for filter in filters])}
-
-	if len(components) == 0:
-		return
 
 	tid_width = max([len(key) for key in traces.keys()])
 	cid_width = max([len(c["id"]) for c in components])
@@ -148,45 +159,102 @@ def main(argv):
 	part_width = max([len(c["part"]) + len(c["type"]) + 1 for c in components])
 	single_mode = (len(components) == 1)
 
-	components.sort(key = lambda x: x["id"][0] + x["id"][1:].zfill(4))
-
-	if graphics:
-		matplotlib.rcParams['figure.figsize'] = (14.0, 9.0)
-		data = image.imread("./pictures/a3-board-clear.jpg")
-		pyplot.axis("off")
-		pyplot.imshow(data)
-		gca = pyplot.gca()
-
 	for c in components:
 		id = c["id"]
-		print("{}: {} {}{}".format( \
-			id.rjust(cid_width),  \
-			(c["part"] + ("" if c["type"] == "" else "/" + c["type"])).ljust(part_width), \
-			c["location"].ljust(3), "" if single_mode else format_pins(c["pins"])))
+		print(format_component(id, c, cid_width, part_width, single_mode, sequential_filter))
 		if sequential_filter or single_mode:
 			print()
 			for pin, t in enumerate(c["pins"]):
-				fmt = "-".rjust(id_width) if t == "" else format_trace(t, traces[t], id_width)
+				fmt = "-".rjust(id_width) if t == "" else format_trace(t, traces[t], id_width, single_mode)
 				print("{:>2}: {}".format(pin + 1, fmt))
 			print()
-		if graphics and "box" in c:
-			colors = { "U" : "#00ff00", \
-					   "L" : "#ff0000", \
-					   "J" : "#ffff00", \
-					   "Q" : "#ff0000"}
-			b = c["box"]
-			t = id[0]
-			color = colors[t] if t in colors else "#000000"
-			rect = Rectangle((b[0], b[1]), b[2], -b[3], linewidth = 1.5, edgecolor = color, facecolor = color + "40")
-			gca.add_patch(rect)
+		draw_component(id, c, gca)
 
 	if not single_mode and not sequential_filter: 
 		print()
 		for k, v in traces.items():
-			print(format_trace(k, v, id_width))
+			print(format_trace(k, v, id_width, single_mode))
 		print()
 
-	if graphics:
+def print_traces(board, trace_filter, sequential_filter, gca):
+	print()
+	filters = re.split(",", trace_filter)
+	traces = {key : board.traces[key] for key in board.traces.keys() \
+			if any([fnmatch.fnmatch(key, filter) for filter in filters])}
+	if len(traces) == 0:
+		return
+
+	single_mode = (len(traces) == 1)
+	tid_width = max([len(key) for key in traces.keys()])
+
+	for key, tr in sorted(traces.items()):
+		tr.sort(key = lambda x: x[0] + str(x[1]).zfill(3))
+		print(format_trace(key, tr, tid_width, single_mode))
+
+		cs = [board.components[i[0]] for i in tr]
+		cid_width = max([len(c["id"]) for c in cs])
+		part_width = max([len(c["part"]) + len(c["type"]) + 1 for c in cs])
+
+		if sequential_filter or single_mode:
+			print()
+			for c in cs:
+				id = c["id"]
+				print(format_component(id, c, cid_width, part_width, single_mode, sequential_filter))
+				draw_component(id, c, gca)
+
+	print()
+
+def main(argv):
+	try:
+		opts, args = getopt.getopt(argv, "ghj:c:t:s",["graphics", "help","json=","component=","trace=", "sequential"])
+	except getopt.GetoptError:
+		usage()
+
+	if len(opts) == 0:
+		usage()
+
+	json_file = "./original/a3-wire-list.json"
+	component_filter = None
+	trace_filter = None
+	sequential_filter = False
+	gca = None
+
+	for opt, arg in opts:
+		if opt in ["-h", "--help"]:
+			usage()
+		elif opt in ["-c", "--component"]:
+			component_filter = arg.upper()
+		elif opt in ["-t", "--trace"]:
+			trace_filter = arg.upper()
+		elif opt in ["-j", "--json"]:
+			json_file = arg
+		elif opt in ["-s", "--sequential"]:
+			sequential_filter = True
+		elif opt in ["-g", "--graphics"]:
+			matplotlib.rcParams['figure.figsize'] = (14.0, 9.0)
+			data = image.imread("./pictures/a3-board-clear.jpg")
+			data = numpy.flipud(data)
+			pyplot.axis("off")
+			pyplot.imshow(data, origin = "lower")
+			gca = pyplot.gca()
+
+	board = load_json(json_file)
+	if board is None: 
+		return
+
+	if component_filter is None and trace_filter is None:
+		usage()
+
+	if component_filter is not None and trace_filter is not None:
+		print("Define only one: -c or -t")
+		return
+
+	if component_filter is not None:
+		print_components(board, component_filter, sequential_filter, gca)
+	elif trace_filter is not None:
+		print_traces(board, trace_filter, sequential_filter, gca)
+
+	if gca is not None:
 		pyplot.show()
 
 if __name__ == "__main__":
